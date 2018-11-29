bed2077b61d210c2d022d22fc2d9a80f756dee1c
David <35275445+DavidGren@users.noreply.github.com>
2018-11-30 00:20:40 +0100

    Base configuration
    - added login, singin and home page

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wraper">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
